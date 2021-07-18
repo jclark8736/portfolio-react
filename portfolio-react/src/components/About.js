830e4ee968f86d58c4d869592d808abf49742e55
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-07-18 19:12:04 -0400

    navbar and app set up

---
+++ portfolio-react/src/components/About.js
@@ -0,0 +1,15 @@
+const About = () => {
+
+
+    return ( 
+<section>
+  <h3>About Me</h3>
+  <p>I am a software developer from New Jersey. I have work experience in a support role in IT (as administrator for a fashion company running cutting machines, industrial plotters and a large group of designers running CAD software) and I am making the switch into development. I'm interested in contributing to open source software, user privacy, and learning everything I can about programming. </p>
+  <br />
+  <p> Please view some of my ongoing and previous projects. Some elements are server backends and so only link to repositories, while the weather tracker, congressional bill tracker and tech blog are deployed projects that you can interact with.
+    I'm extremely proud of the Congressional Bill Tracker, which uses public data and allows you to track the progress of bills in the US Congress.</p>
+</section>
+    )}
+
+
+export default About
---
+++ portfolio-react/src/components/Navbar.js
@@ -0,0 +1,21 @@
+const Navbar = () => {
+
+
+    return ( 
+    <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+       <h3>John Clark</h3>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#nav">Contact</a></li>
+        <li><a href="#begin-project">Coding Projects</a></li>
+        <li><a href="#resume">Resume</a></li>
+      </ul>
+    </div>
+    </nav>
+    )}
+    
+    
+      export default Navbar;
