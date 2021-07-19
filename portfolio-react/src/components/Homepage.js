12f89dc3f4dde4b338bcd967c3146cb2b4fee684
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-07-19 02:19:21 -0400

    trying to implement switch

--- portfolio-react/src/components/Homepage.js
+++ portfolio-react/src/components/Homepage.js
@@ -10,45 +10,98 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import Examples from './Examples'
 
-export default function Homepage() {
+
+
+const Homepage = () => {
     const [currentView, setCurrentView] = useState('Home');
 
     const renderPage = () => {
-    if (currentView === 'Home') {
-
-        return(
-        <Fragment>
-            <Navbar />
-            <About />
-        </Fragment>
-        );
-    }
-    if (currentView === 'Contact') {
-        return(
-        <Fragment>
-            <Navbar />
-            <Footer />
-        </Fragment>
-        );
-    }
-    if (currentView === "Coding") {
-        return(
-        <Fragment>
-            <Navbar />
-            <Examples />
-        </Fragment>
+        switch (currentView) {
+            case "Home":
+    
+                        return(
+            <Fragment>
+                <Navbar />
+                <About />
+            </Fragment>
+            );
+            case "Contact":
+                return(
+                    <Fragment>
+                    <Navbar />
+                    <Contact />
+                </Fragment>
+                )
+                case "Coding":
+                    return(
+                        <Fragment>
+                        <Navbar />
+                        <Examples />
+                    </Fragment>
+                    )
+                default:
+                    return (
+                        <Fragment>
+                        <Navbar />
+                        <About />
+                    </Fragment>  
+                    )
+        }
+        return (
+            <div>
+                {setCurrentView(currentView)}
+            </div>
         )
     }
-    else {
-        return(
-        <Fragment>
-            <Navbar />
-            <About />
-        </Fragment>
-        )
     }
+  
+export default Homepage
 
-    const handleViewChange = (view) => setCurrentView(view)
 
-}}
 
+// export default function Homepage() {
+    
+//     const [currentView, setCurrentView] = useState('Home');
+    
+//     const renderPage = () => {
+//     if (currentView === 'Home') {
+
+//         return(
+//         <Fragment>
+//             <Navbar />
+//             <About />
+//         </Fragment>
+//         );
+//     }
+//     if (currentView === 'Contact') {
+//         return(
+//         <Fragment>
+//             <Navbar />
+//             <Footer />
+//         </Fragment>
+//         );
+//     }
+//     if (currentView === "Coding") {
+//         return(
+//         <Fragment>
+//             <Navbar />
+//             <Examples />
+//         </Fragment>
+//         )
+//     }
+//     else {
+//         return(
+//         <Fragment>
+//             <Navbar />
+//             <About />
+//         </Fragment>
+//         )
+//     }
+
+//     const handleViewChange = (view) => setCurrentView(view)
+
+// }}
+// 
+// 
+// 
+// 
